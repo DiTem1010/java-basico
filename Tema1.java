255794d5e818923e9f7f8c56c212b0f8f3611aa6
Diglas Temoche <104596485+DiTem1010@users.noreply.github.com>
2022-06-19 16:20:38 -0500

    Tipos de datos y creación de variables

---
+++ Tema1.java
@@ -0,0 +1,40 @@
+package com.exercises;
+
+public class Tema1 {
+    public static void main(String[] args) {
+        //Imprimiendo tipos enteros
+
+        byte var1 = 2;
+        System.out.println("Mi numero byte es: " + var1);
+
+        short var2 = 6;
+        System.out.println("Mi numero short es: " + var2);
+
+        int numero = 10;
+        System.out.println("El numero escrito es: " + numero);
+
+        long num_largo= 15000;
+        System.out.println("El numero long escrito es: " + num_largo);
+
+        //Imprimiendo tipos decimales
+        float var3 = 6.4f;
+        System.out.println("El numero float es: " + var3);
+
+        double var4 = 10.5d;
+        System.out.println("El numero decimal es: " + var4);
+
+        //Imprimiendo tipos booleano
+        boolean verdadero = true;
+        System.out.println("El tipo boleano verdadero se escribe: " + verdadero);
+
+        boolean falso = false;
+        System.out.println("El tipo booleano falso se escribe: " + false);
+
+        //Imprimiendo tipo texto
+        char letra = 't';
+        System.out.println("El tipo de dato char se escribe con una letra: " + letra);
+
+        String mensaje = "Hola Mundo";
+        System.out.println("El mensaje de inicio en la programacion es: " + mensaje);
+    }
+}
