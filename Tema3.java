0f5c1d6ca25faccf1b7f29792acc6136426daf86
Diglas Temoche <104596485+DiTem1010@users.noreply.github.com>
2022-07-09 12:31:23 -0500

    Create Tema3.java

---
+++ Tema3.java
@@ -0,0 +1,11 @@
+package com.exercises;
+
+public class Tema3 {
+    public static void main(String[] args) {
+        String[] nombres = new String[]{"Cecilia","Marcos","Daniela","Diglas"};
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+        }
+    }
+}
