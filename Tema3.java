ada3036f52afae93b10357ad1aee2fa49411580b
Diglas Temoche <104596485+DiTem1010@users.noreply.github.com>
2022-07-09 18:26:45 -0500

    Aplicando Arrays
    uso de arrays con la clase String

--- Tema3.java
+++ Tema3.java
@@ -2,7 +2,7 @@ package com.exercises;
 
 public class Tema3 {
     public static void main(String[] args) {
-        String[] nombres = new String[]{"Cecilia","Marcos","Daniela","Diglas"};
+        String[] nombres = new String[]{"Ariana","Lucas","Camila","Diglas"};
 
         for (int i = 0; i < nombres.length; i++) {
             System.out.println(nombres[i]);
