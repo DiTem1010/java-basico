2f7834923d0e567c436ee0e7b8b939f2cacdb76a
Diglas Temoche <104596485+DiTem1010@users.noreply.github.com>
2022-07-09 19:55:52 -0500

    Uso de interfaces

---
+++ Tema5.java
@@ -0,0 +1,47 @@
+package com.exercises;
+
+public class Tema5 {
+
+    public interface CocheCRUD {
+
+        void save();
+        void findAll();
+        void delete();
+        
+    }
+
+    public static class CocheCRUDImpl implements CocheCRUD {
+
+       
+        public void save() {
+        }
+
+        
+        public void findAll() {
+        }
+
+      
+        public void delete() {
+        }
+
+        String save = "Guardar";
+        String findAll = "Buscar todo";
+        String delete = "Borrar";
+
+        @Override
+        public String toString() {
+            return "CocheCRUDImpl" +
+                    " save=" + save +
+                    ", findAll=" + findAll +
+                    ", delete=" + delete;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        CocheCRUD cocheCrud = new CocheCRUDImpl();
+        System.out.println(cocheCrud);
+
+    }
+
+}
